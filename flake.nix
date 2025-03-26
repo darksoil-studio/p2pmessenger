c8decb8115919b9daeacb5a7448715386e970ba7
guillem.cordoba <guillem.cordoba@gmail.com>
2025-03-26 11:25:31 +0100

    pnpm

--- flake.nix
+++ flake.nix
@@ -3,21 +3,17 @@
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      systems =
+        [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
-
         # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        devShells.default = pkgs.mkShell {
-          packages = [pkgs.nodejs_20];
-        };
+        devShells.default =
+          pkgs.mkShell { packages = [ pkgs.nodejs_20 pkgs.pnpm ]; };
       };
     };
 }
